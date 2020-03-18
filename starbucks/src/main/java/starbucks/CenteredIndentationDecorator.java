4f4229f9c84f439f1123307de2c513282b675080
Aditi Mangal <mangsaditi2610@gmail.com>
2020-03-17 18:20:17 -0700

    Center Indentation for keypad fixed

--- starbucks/src/main/java/starbucks/CenteredIndentationDecorator.java
+++ starbucks/src/main/java/starbucks/CenteredIndentationDecorator.java
@@ -1,36 +1,40 @@
 package starbucks;
 
-/** Centered Indentation for Decorator**/
+/** Centered Indentation for Decorator **/
 public class CenteredIndentationDecorator extends IndentationDecorator {
-	 
+
 	/**
-     * Return Display Component Contents
-     * @return Display Component Contents
-     */
+	 * Return Display Component Contents
+	 * 
+	 * @return Display Component Contents
+	 */
 	@Override
 	public String display() {
-		
-		StringBuffer value= new StringBuffer();
 
-		System.err.println("CenteredIndentationDecorator => Screen Width is:"+ this.width);
-		
+		StringBuffer value = new StringBuffer();
+
+		System.err.println("CenteredIndentationDecorator => Screen Width is:" + this.width);
+
 		if (this.screenContents == null || this.screenContents.isEmpty()) {
 			return "";
 		}
 		String lines[] = this.screenContents.split("\\r?\\n");
-		for(String line: lines) {
+		for (String line : lines) {
 			if (line == "\n")
 				value.append("\n");
 			else {
 				int w = line.length();
-				int space = (this.width-w)/2;
-				value.append(this.padSpaces(space+1));
-				value.append(line);
-				value.append("\n");			
+				if (w < 13) {
+					int space = (this.width - w) / 2;
+					value.append(this.padSpaces(space + 1));
+				}
+					value.append(line);
+					value.append("\n");
+				
 			}
 		}
-	
-        return value.toString();
-	
+
+		return value.toString();
+
 	}
 }
