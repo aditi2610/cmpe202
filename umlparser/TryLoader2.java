fd778313707596d201e449d48eeefad3509f8028
Aditi Mangal <mangsaditi2610@gmail.com>
2020-04-18 17:52:35 -0700

    test case 3 try

--- umlparser/TryLoader2.java
+++ umlparser/TryLoader2.java
@@ -5,4 +5,13 @@ public class TryLoader2 extends JavaParserBaseListener {
     public void enterClassDeclaration(JavaParser.ClassDeclarationContext ctx) {
         System.out.println("Context is:" + ctx.getText());
     }
+
+    void exitImportDeclaration(JavaParser.ImportDeclarationContext ctx) {
+        ctx.enterRule(listener).enterClassDeclaration();
+    }
+
+    public void enterTypeDeclaration(JavaParser.TypeDeclarationContext ctx) {
+
+        ctx.getRuleIndex().ClassDeclarationContex;
+    }
 }
