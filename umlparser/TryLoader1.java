04f44f835f91ce7333c32cda7e5b5242473121f9
Aditi Mangal <mangsaditi2610@gmail.com>
2020-04-19 21:05:49 -0700

    test 1 able to generate YUML digram through this

--- umlparser/TryLoader1.java
+++ umlparser/TryLoader1.java
@@ -4,13 +4,16 @@ import java.util.List;
 public class TryLoader1 extends JavaParserBaseListener {
 
     static String className = "";
-    static List<String> list;
-    static String primitiveString;
+    static List<String> classVariablelist;
+    // static String primitiveString;
     int flagForCollection = 0;
     String element = "";
+    String classVariables = "";
     static List<String> extendingClasses;
     static List<String> implementingClasses;
     static List<String> usesList;
+    List<String> memberVariables;
+    List<String> primitiveVariables;
 
     @Override
     public void enterClassDeclaration(JavaParser.ClassDeclarationContext ctx) {
@@ -26,6 +29,7 @@ public class TryLoader1 extends JavaParserBaseListener {
         System.out.println("ClassName: " + className);
         extendingClasses = new ArrayList<String>();
         implementingClasses = new ArrayList<String>();
+        primitiveVariables = new ArrayList<String>();
         usesList = new ArrayList<String>();
         if (ctx.getText().contains("extends")) {
             extendingClasses.add(ctx.typeType().classOrInterfaceType().getText());
@@ -37,8 +41,65 @@ public class TryLoader1 extends JavaParserBaseListener {
             }
 
         }
-        list = new ArrayList<String>();
-        primitiveString = "";
+        classVariablelist = new ArrayList<String>();
+        // primitiveString = "";
+
+    }
+
+    @Override
+    public void enterClassBodyDeclaration(JavaParser.ClassBodyDeclarationContext ctx) {
+        element = "";
+        classVariables = "";
+    }
+
+    @Override
+    public void enterMemberDeclaration(JavaParser.MemberDeclarationContext ctx) {
+        flagForCollection = 0;
+        // element = "";
+    }
+
+    @Override
+    public void enterClassOrInterfaceModifier(JavaParser.ClassOrInterfaceModifierContext ctx) {
+        if (ctx.getText().equals("private")) {
+            element = "-";
+        }
+    }
+
+    @Override
+    public void enterFieldDeclaration(JavaParser.FieldDeclarationContext ctx) {
+        // String element = "";
+        JavaParser.ClassOrInterfaceTypeContext classOrInterface = ctx.typeType().classOrInterfaceType();
+        if (null != classOrInterface) {
+            if (classOrInterface.getText().contains("Collection")) {
+                classVariables += "1-0..*";
+                classVariables += "["
+                        + classOrInterface.typeArguments().get(0).typeArgument().get(0).typeType().getText() + "]";
+
+            } else {
+                classVariables += "-[" + classOrInterface.getText() + "]";
+            }
+            // System.out.println("Class Variable: " + classVariables);
+            classVariablelist.add(classVariables);
+        }
+        if (ctx.typeType().primitiveType() != null) {
+            String pElement = " ";
+
+            element += ctx.variableDeclarators().variableDeclarator(0).variableDeclaratorId().getText();
+            element += ":" + ctx.typeType().primitiveType().getText();
+            if ((ctx.typeType().getText().charAt(ctx.typeType().getText().length() - 1)) == (']')) {
+                element += "(*)";
+            }
+            element += ";";
+            // System.out.println("Primitive type is: " + element);
+            primitiveVariables.add(element);
+        }
+
+    }
+
+    @Override
+    public void enterFormalParameterList(JavaParser.FormalParameterListContext ctx) {
+        // System.out.println(ctx.getText());
+        usesList.add(ctx.formalParameter(0).typeType().classOrInterfaceType().getText());
 
     }
 
@@ -53,62 +114,18 @@ public class TryLoader1 extends JavaParserBaseListener {
         for (String useClasses : usesList) {
             System.out.println("[" + className + "]uses -.->[" + useClasses + "]");
         }
-        for (int i = 0; i < list.size(); i++) {
+        for (int i = 0; i < classVariablelist.size(); i++) {
             if (i == 0) {
-                System.out.println("[" + className + " |" + primitiveString + "]" + list.get(i));
+                System.out.print("[" + className + "|");
+                for (String primitive : primitiveVariables) {
+                    System.out.print(primitive);
+                }
+                System.out.println("]" + classVariablelist.get(0));
+
             } else {
-                System.out.println("[" + className + "]" + "-" + list.get(i));
+                System.out.println("[" + className + "]" + classVariablelist.get(i));
             }
         }
-
-    }
-
-    @Override
-    public void enterMemberDeclaration(JavaParser.MemberDeclarationContext ctx) {
-        flagForCollection = 0;
-        element = "";
-    }
-
-    @Override
-    public void enterClassOrInterfaceModifier(JavaParser.ClassOrInterfaceModifierContext ctx) {
-        if (ctx.getText().equals("private")) {
-            element = "-";
-        }
-    }
-
-    @Override
-    public void enterFieldDeclaration(JavaParser.FieldDeclarationContext ctx) {
-
-        if (ctx.typeType().primitiveType() != null) {
-            primitiveString += element;
-            primitiveString += ctx.variableDeclarators().variableDeclarator(0).variableDeclaratorId().getText();
-            primitiveString += ":" + ctx.typeType().primitiveType().getText();
-            if ((ctx.typeType().getText().charAt(ctx.typeType().getText().length() - 1)) == (']')) {
-                primitiveString += "(*)";
-            }
-            primitiveString += ";";
-        }
-    }
-
-    @Override
-    public void enterClassOrInterfaceType(JavaParser.ClassOrInterfaceTypeContext ctx) {
-        String element = "";
-        if (ctx.getText().contains("Collection")) {
-            element += "1-0..*";
-            element += "[" + ctx.typeArguments().get(0).typeArgument().get(0).typeType().getText() + "]";
-            flagForCollection = 1;
-            list.add(element);
-        } else if (flagForCollection == 0) {
-            element += "1[" + ctx.getText() + "]";
-            list.add(element);
-        }
-
-    }
-
-    @Override
-    public void enterFormalParameterList(JavaParser.FormalParameterListContext ctx) {
-        // System.out.println(ctx.getText());
-        usesList.add(ctx.formalParameter(0).typeType().classOrInterfaceType().getText());
 
     }
 
